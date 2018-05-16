7ef7f16df7944731b25ddd827822433addee19b3
gabrielloliveira <gabrielloliveira097@gmail.com>
2018-05-16 17:13:39 -0300

    Adicionado função para exibir os personagens

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // Declarando a estrutura para o tipo personagem de forma global
 struct personagem{
@@ -12,19 +13,21 @@ struct personagem{
 };
 struct personagem jogador[50];
 
+// Função para o algoritmo 'pausar' a execução enquando o usuário não digitar um enter
 void pause(){
   printf("Aperte enter para continuar...\n");
   char buffer[80];
   fgets(buffer, sizeof buffer, stdin);
   getchar();
+  void menu();
+  menu();
 }
 
+// Função que cria o personagem que vai ser controlado pelo usuário
 void criarPersonagem(){
   if(jogador[0].vida == 100){
     printf("Você já criou seu personagem. Comece o jogo\n");
     pause();
-    void menu();
-    menu(); 
   }else{
     jogador[0].vida = 100;
     jogador[0].especial = 50;
@@ -65,9 +68,90 @@ void criarPersonagem(){
     // printf("nome: %s\nvida: %d\nataque: %d\nespecial: %d\nescudo: %d\n", jogador[0].nome, jogador[0].vida, jogador[0].ataque, jogador[0].especial, jogador[0].defesa);
     printf("Parabéns! Você criou seu personagem\n");
     pause();
-    void menu();
-    menu();
   }  
+}
+
+// Mostra todos personagens criados
+void exibirPersonagens(){
+  // Verificação para saber se existe os outro jogadores
+  if(jogador[1].vida != 100){
+    // Criando os jogadores inimigos
+    char possiveisNomes[50][255] = {
+      " ",
+      "Mago Mutante",
+      "Gatinho Fofo",
+      "SPP",
+      "Troll de Gelo",
+      "Feiticeira Sombria",
+      "Goblim",
+      "Caçador de bruxos",
+      "Arqueiro",
+      "Fantasma",
+      "Coração cremoso",
+      "Eu sou seu pai",
+      "Prova de algoritmo",
+      "Hu3Hu3",
+      "Cavaleiro solitário",
+      "The little boss",
+      "Oi sumida rs",
+      "Café com leite",
+      "Mercado de trabalho",
+      "Essa barra que é gostar de você",
+      "ENEM",
+      "Máscara",
+      "Gurú do Himalaia",
+      "Java",
+      "Usuário Final",
+      "Bug Escondido",
+      "Scooby Doo",
+      "Ben 10",
+      "Tundercats",
+      "Goku",
+      "Vegeta",
+      "Bills",
+      "Jiren",
+      "Whis",
+      "Zen oh",
+      "Yugi",
+      "Tio do jackie chan",
+      "Talismã do dragão",
+      "Megazord",
+      "Shenlong",
+      "Thanos",
+      "Peter do DeadPool",
+      "Logan",
+      "Homer",
+      "Bart",
+      "Lisa",
+      "Marge",
+      "Lindinha",
+      "Florzinha",
+      "Docinho"
+    }; 
+    
+    for(int i = 1; i < 50; ++i){
+      jogador[i].vida = 100;
+      jogador[i].especial = 50;
+      jogador[i].ataque = (rand()%29) + 1;
+      jogador[i].defesa = (rand()%19) + 1;
+      strcpy(jogador[i].nome, possiveisNomes[i]);
+    }
+  }
+  if(jogador[0].vida == 100){
+    printf("+-----------Jogadores------------+\n");
+    for (int i = 0; i < 50; ++i){
+      printf("| %d - %s\n", i+1, jogador[i].nome);
+    }
+    printf("+--------------------------------+\n");
+  }else{
+    // Mostra somente os jogadores controlados pelo jogo
+    printf("+-----------Jogadores------------+\n");
+    for (int i = 1; i < 50; ++i){
+      printf("| %d - %s\n", i, jogador[i].nome);
+    }
+    printf("+--------------------------------+\n");
+  }
+  pause();
 }
 
 void comecarJogo(){
@@ -76,8 +160,6 @@ void comecarJogo(){
   }else{
     printf("Antes de iniciar o jogo você deve criar o seu personagem!\n");
     pause();
-    void menu();
-    menu();
   }
 }
 
@@ -103,7 +185,7 @@ void menu(){
       criarPersonagem();
       break;
     case 3:
-      // exibirPersonagens();
+      exibirPersonagens();
       break;
     case 4:
       // editarPersonagens();
@@ -118,6 +200,7 @@ void menu(){
 }
 
 int main(){
+  srand(time(NULL));
   printf("Bem-vindo(a) ao melhor jogo de todos os tempos da última semana!\n");
   menu();
 }
